c3a56198f06f0e82abf866fb0035496236760450
Matheus Vieira <matheus.vieira@primeup.com.br>
2023-03-25 12:13:21 -0300

    java servlet initial structure

---
+++ src/main/java/MyServlet.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+        String json = "{\"status\": \"server running\"}";
+        response.getWriter().write(json);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
